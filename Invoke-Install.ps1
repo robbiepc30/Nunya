6a5f0c2e43da76e136f0151e099fad745537cd7a
Robert Courtney <robbiepc30@hotmail.com>
2016-12-02 09:59:33 -0900

    Update InvokeInstall module

---
+++ Invoke-Install.ps1
@@ -0,0 +1,40 @@
+﻿<#
+.Synopsis
+   Short description
+.DESCRIPTION
+   Long description
+.EXAMPLE
+   Example of how to use this cmdlet
+.EXAMPLE
+   Another example of how to use this cmdlet
+#>
+function Invoke-Install
+{
+    [CmdletBinding()]
+    Param
+    (
+        [String]$Path = '.'
+    )
+    # explicitly make this var an array by incolsing it in a @()
+    # this protects the foreach loop from throwing an error if only one install script exist, because the var obj would not be an array type if only one *install.ps1 file exist
+    $installerScripts = @(Get-ChildItem -Path $Path -Recurse -Include *.Install.ps1)
+
+    $otherErrorCodes = @{ -2145124329 = "This update is not applicable to your computer"
+                          2359302 = "This update is already installed on this computer"
+                        }
+
+    foreach ($i in $installerScripts) {
+        
+        $installerName = (Split-Path -Path $i.FullName -Leaf) -replace ".Install.ps1", ""
+        Write-Progress -Activity "Installing updates" -Status ("{0:P1}" -f $($installerScripts.IndexOf($i)/$installerScripts.Length)) -CurrentOperation "Installing $installerName"  -PercentComplete (($installerScripts.IndexOf($i)/$installerScripts.Length) * 100)
+        Write-Verbose "Installing $installerName"
+        & $i.FullName
+        if ($otherErrorCodes.ContainsKey($LASTEXITCODE)) {
+            Write-Verbose " `t`t: $($otherErrorCodes.$LASTEXITCODE) , Exit code: $LASTEXITCODE" 
+        } 
+        else {
+            Write-Verbose " `t`t : $([ComponentModel.Win32Exception]$LASTEXITCODE) , Exit code: $LASTEXITCODE"
+        }
+    }
+}
+
--- New-InstallFile.ps1
+++ New-InstallFile.ps1
@@ -1,16 +1,4 @@
-﻿$name = "Windows6.1-KB3163245-x64.msu"
-
-
-
-
-
-function createFile ($Name, $Content)
-{
-    Set-Content -Path $Name -Value $Content -Encoding UTF8
-}
-    
-
-<#
+﻿<#
 .Synopsis
    Short description
 .DESCRIPTION
@@ -39,8 +27,9 @@ function New-InstallFile
         $fileNameList = Get-ChildItem -Path $Path -Include *.msu -Recurse | select -ExpandProperty FullName
  
         foreach ($f in $fileNameList) {
+                
                 $installerFileName = Split-Path -Path $f -Leaf
-                $installerFileName
+
                 $code = '# Set Location to the path of this script
                         $currentPath = Split-Path -parent $MyInvocation.MyCommand.Definition
                         Set-Location -Path $currentPath
@@ -48,17 +37,18 @@ function New-InstallFile
                         # Invoke Installer
                         $process = Start-Process  WUSA -ArgumentList "#MSUFile# /quiet /norestart" -Wait -PassThru
                         Exit $process.ExitCode' -replace "#MSUFile#", $installerFileName
-                $code
 
                 $ps1InstallFileName = $f + ".Install.ps1"
-                $ps1InstallFileName
-            #createFile -Name $f -Content $code
+
+                createFile -Name $ps1InstallFileName -Content $code
         }
     }
 }
 
-(Split-Path -Path $f -Leaf) -replace '.Install.ps1', ''
-
+function createFile ($Name, $Content)
+{
+    Set-Content -Path $Name -Value $Content -Encoding UTF8
+}
 
 
 
