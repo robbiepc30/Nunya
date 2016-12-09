073634ef73b03b6f3b2f709f3e4d346a0e455f19
Robert Courtney <robbiepc30@hotmail.com>
2016-12-08 16:03:14 -0900

    Updated InvokeInstall Module to include inclue x/x in progress bar

--- Invoke-Install.ps1
+++ Invoke-Install.ps1
@@ -26,7 +26,7 @@ function Invoke-Install
     foreach ($i in $installerScripts) {
         
         $installerName = (Split-Path -Path $i.FullName -Leaf) -replace ".Install.ps1", ""
-        Write-Progress -Activity "Installing updates" -Status ("{0:P1}" -f $($installerScripts.IndexOf($i)/$installerScripts.Length)) -CurrentOperation "Installing $installerName"  -PercentComplete (($installerScripts.IndexOf($i)/$installerScripts.Length) * 100)
+        Write-Progress -Activity "Installing updates" -Status ("$($installerScripts.IndexOf($i))`/$($installerScripts.Length), {0:P1}" -f $($installerScripts.IndexOf($i)/$installerScripts.Length)) -CurrentOperation "Installing $installerName"  -PercentComplete (($installerScripts.IndexOf($i)/$installerScripts.Length) * 100)
         Write-Verbose "Installing $installerName"
         & $i.FullName
         if ($otherErrorCodes.ContainsKey($LASTEXITCODE)) {
