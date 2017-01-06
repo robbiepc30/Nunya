774c258f735357b414f59b052c4786e9f81b11a0
Robbie Courtney <robbiepc30@hotmail.com>
2017-01-06 11:44:12 -0900

    added test, update install-nunyapackage

---
+++ Functions/Install-NunyaPackage.Tests.ps1
@@ -0,0 +1,33 @@
+﻿$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
+. "$here\$sut"
+
+#Act, Arrange, Assert
+Describe "Install-NunyaPackage" {
+    
+    Mock Start-Process {}
+    It "throws error if file does not exist" {
+        #Arrange
+        $fileThatDoesNotExist = "DoesNotExist.exe"
+        $throwError = "`"$fileThatDoesNotExist`" file cannot be found. Check the -FilePath parameter and try again"
+        #Act & Assert
+        { Install-NunyaPackage -FilePath $fileThatDoesNotExist -SilentArgs "/S" }| Should throw $throwError
+    }
+
+    Context "Test switch for different install types .msi, .msu, and .exe" {
+        Mock Test-Path { $true }
+        It "Should execute msi switch block" {
+            #Arrange
+            #Act
+            $result = Install-NunyaPackage -FilePath "c:\test.msi" -SilentArgs "/quiet /norestart" -Verbose 4>&1
+            #Assert
+            #Write-Verbose "Somthing is installing" -Verbose 4>&1 | Should Match "installing"
+            $result | Should Match "installing"
+        }
+
+    }
+    
+
+
+    
+}
--- Functions/Install-NunyaPackage.ps1
+++ Functions/Install-NunyaPackage.ps1
@@ -1,4 +1,5 @@
 function Install-NunyaPackage {
+
     [CmdletBinding()]
     param(
         [Parameter(Mandatory=$true)]
@@ -12,20 +13,25 @@ function Install-NunyaPackage {
     # Setup loging directory and log paths
     $nunyaLogDirectory = Join-Path $env:temp "Nunya"
     $filnameWOExtention = [System.IO.Path]::GetFileNameWithoutExtension($FilePath)
+    $filename = [System.IO.Path]::GetFileName($FilePath)
     $installerLogDirectory = Join-Path $nunyaLogDirectory $filnameWOExtention
     if (!(Test-Path $installerLogDirectory)) { New-Item -Path $installerLogDirectory -ItemType Directory | Out-Null }
     $stdOutLog = Join-Path $installerLogDirectory "stdOut.log"
     $stdErrLog = Join-Path $installerLogDirectory "stdErr.log"
 
     $fileType = [System.IO.Path]::GetExtension($FilePath).Replace(".", "")
-
+    
     switch ($fileType) {
         "msi" 
         {  
             $InstallLogPath = Join-Path $installerLogDirectory "install.log"
             $msiArgs = "/i `"$FilePath`" /l*vx `"$InstallLogPath`" $SilentArgs"
+            Write-Debug "Starting MSI installer:  $env:SystemRoot\System32\msiexec.exe with Arguments: $msiArgs"
+            Write-Verbose "Installing $filename..."
             $process = Start-Process -FilePath "$env:SystemRoot\System32\msiexec.exe" -ArgumentList $msiArgs -Wait -PassThru -RedirectStandardError $stdErrLog -RedirectStandardOutput $stdOutLog
-            return $process.ExitCode
+            $exitCode = $process.ExitCode
+            # return exit code if not null or empty
+            if ($process.ExitCode) { return $process.ExitCode }
         }
         "msu" 
         {
@@ -33,18 +39,20 @@ function Install-NunyaPackage {
             $msuArgs = "`"$FilePath`" /log:`"$InstallLogPath`" $SilentArgs"
 
             $process = Start-Process -FilePath "$env:SystemRoot\System32\wusa.exe" -ArgumentList $msuArgs -Wait -PassThru -RedirectStandardError $stdErrLog -RedirectStandardOutput $stdOutLog
-            return $process.ExitCode
+            # return exit code if not null or empty
+            if ($process.ExitCode) { return $process.ExitCode }
         }
         "exe" 
         {
             $process = Start-Process -FilePath "$FilePath" -ArgumentList $SilentArgs -Wait -PassThru -RedirectStandardError $stdErrLog -RedirectStandardOutput $stdOutLog
-            return $process.ExitCode
+            $exitCode = $process.ExitCode
+            # return exit code if not null or empty
+            if ($process.ExitCode) { return $process.ExitCode }
         }
         Default { throw "Unknown file type `".$fileType`" , Install-NunyaPackage can install, .msi, .msu, or .exe file types"}
     }
 }
 
-
 #Install-NunyaPackage -FilePath "C:\Users\robert.p.courtney\Desktop\Win7x6-Client-Image-Patches\Win7x64\MS16-087\Windows6.1-KB3170455-x64.msu" -SilentArgs "/quiet /norestart"
-Install-NunyaPackage -FilePath "c:test.exe" -SilentArgs "/s"
+#Install-NunyaPackage -FilePath "c:test.exe" -SilentArgs "/s"
 
