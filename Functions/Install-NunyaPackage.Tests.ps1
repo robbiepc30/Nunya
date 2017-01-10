ec94d8816659bdf1d05d8d50ac9d0e7eb5cfbed4
Robbie Courtney <robbiepc30@hotmail.com>
2017-01-09 16:48:06 -0900

    updated test, still playing around

--- Functions/Install-NunyaPackage.Tests.ps1
+++ Functions/Install-NunyaPackage.Tests.ps1
@@ -13,21 +13,37 @@ Describe "Install-NunyaPackage" {
         #Act & Assert
         { Install-NunyaPackage -FilePath $fileThatDoesNotExist -SilentArgs "/S" }| Should throw $throwError
     }
+    function T ($SomeParam, $OtherParam) {
+        Write-Host $SomeParam
+    }
 
-    Context "Test switch for different install types .msi, .msu, and .exe" {
+    Context "Test switch for install type .msi" {
+
+        $filePathArg = "c:\test.msi"
+        $filename = [System.IO.Path]::GetFileName($filePathArg)
+        $argumentListArg = "/i `"$filePathArg`" /l*vx `"C:\Users\robert.p.courtney\AppData\Local\Temp\Nunya\$filename\install.log`" /quiet /norestart"
         Mock Test-Path { $true }
+        Mock Start-Process {} -ParameterFilter { ($filePath -eq $filePathArg) -and ($ArgumentList -eq $argumentListArgs) }
+
+        Mock T {} -ParameterFilter { ($SomeParam -eq "t") -and ($OtherParam -eq "o")}
         It "Should execute msi switch block" {
             #Arrange
             #Act
-            $result = Install-NunyaPackage -FilePath "c:\test.msi" -SilentArgs "/quiet /norestart" -Verbose 4>&1
+            $result = Install-NunyaPackage -FilePath $filePathArg -SilentArgs $argumentListArgs -Verbose 4>&1
             #Assert
             #Write-Verbose "Somthing is installing" -Verbose 4>&1 | Should Match "installing"
-            $result | Should Match "installing"
+            $result | Should Match "installing $filename"
+        }
+
+        It "Testing multi parameters with Assert-MockCalled" {
+            #Arrange
+            #Act
+            $r = T -SomeParam "t" "o"
+            #Assert-MockCalled Start-Process -ParameterFilter {($filePath -eq $filePathArg) -and ($ArgumentList -eq $argumentListArgs)} -Exactly 1
+            Assert-MockCalled T -ParameterFilter { ($SomeParam -eq "t") -and ($OtherParam -eq "o")} -Exactly 1
+
         }
 
     }
-    
 
-
-    
 }
